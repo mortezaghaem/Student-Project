3f03e9c56cef59c864b269755b2bde162d203154
MortezaGhaem <mortezaghaem@gmail.com>
2015-03-14 09:23:41 +0330

    This is print 7 days of week

---
+++ 7DaysOfWeek.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+ int main()
+{
+	int Number;
+	printf("Enter Number Between 1 until 7 :\n");
+	scanf(" %d",&Number);
+if ( Number== 1)
+	printf("Day is Saturday\n");
+
+if ( Number== 2)
+	printf("Day is Sunday\n");
+
+	if ( Number== 3)
+	printf("Day is Monday\n");
+
+	if ( Number== 4)
+	printf("Day is Tuesday\n");
+
+	if ( Number== 5)
+	printf("Day is Wendsday\n");
+
+if ( Number== 6)
+	printf("Day is Thursrday\n");
+
+if ( Number== 7)
+	printf("Day is Friday\n");
+
+	return 0;
+}
