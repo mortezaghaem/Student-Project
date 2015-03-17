16e46ae667f45bf78fc77afa3e289b929159e560
MortezaGhaem <mortezaghaem@gmail.com>
2015-03-18 02:28:01 +0330

    Max 2 Numbers With A line Comment

--- Max2Number.c
+++ Max2Number.c
@@ -3,12 +3,15 @@ int main()
 {
 	int Number1;
 	int Number2;
-	printf("Enter Two Numbers :\n");
-	scanf("%d %d",&Number1,&Number2);
-	if (Number1 > Number2)
+	printf("Enter a Number :\n");
+	scanf("%d",&Number1);
+	printf("Enter Another Number :\n");
+	scanf("%d",&Number2);
+	if (Number1 > Number2) 
 		printf("Number1 is Max\n");
 	else 
 		printf("Number2 is Max\n");
 	/* code */
 	return 0;
 }
+// 2 add ra az vorudi migire va moghayese mikone va add bozorgtar moshakhas mikone. 
