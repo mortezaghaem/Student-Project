94492d5cb0f8fa5e8818d92723951b707cdab867
MortezaGhaem <mortezaghaem@gmail.com>
2015-03-18 09:11:53 +0330

    4 amale asli jam-tafrigh-zarb-taghsim

--- 4MainOperatorMath.c
+++ 4MainOperatorMath.c
@@ -1,13 +1,19 @@
 #include "stdio.h"
 int main()
-
+// barname 2 ta aad migire va hasele jam-tafrigh-zarb-taghsim ra neshan midehad.
 {
-	int Number1;
+	int Number1; 
 	int Number2;
-	int Sum,Minus,Multiply,Divide;
 
-	printf("Enter 2 Numbers\n");
-	scanf(" %d %d",&Number1,&Number2);
+	int Sum; // jam 2 add vorudi dar Sum negah dari mishavad.
+	int Minus; // tafrighe  2 add vorudi dar Minus negah dari mishavad.
+	int Multiply; // zarb 2 add vorudi dar Multiply negah dari mishavad.
+	int Divide; // taghsim 2 add vorudi dar Divide negah dari mishavad.
+
+	printf("Enter a Numbers :\n");
+	scanf(" %d",&Number1);
+	printf("Enter Another Number :\n");
+	scanf("%d",&Number2);
 
 	Sum=Number1+Number2;
 	Minus=Number1- Number2;
